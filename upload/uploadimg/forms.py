564618faecbf790020dfb3ba0da8fcf8165c7c4f
Giansalvo Gusinu <46819324+giansalvo@users.noreply.github.com>
2022-02-28 21:54:49 +0100

    initial commit

---
+++ upload/uploadimg/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from uploadimg.models import Image
+
+
+class ImageForm(forms.ModelForm):
+    """Form for the image model"""
+
+    class Meta:
+        model = Image
+        fields = ('title', 'image')
+        
