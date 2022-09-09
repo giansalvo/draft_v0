9b95ba7268bee1d419246d95f9c0d7e830140280
Giansalvo Gusinu <46819324+giansalvo@users.noreply.github.com>
2022-09-09 15:40:53 +0200

    first basic working version

--- upload/uploadimg/views.py
+++ upload/uploadimg/views.py
@@ -1,20 +1,61 @@
-from http.client import HTTPResponse # gians TODELETE DEBUG
+"""
+    Simple project for CNN demonstration
+
+    Copyright (c) 2022 Giansalvo Gusinu
+
+    Permission is hereby granted, free of charge, to any person obtaining a 
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
 from django.http import HttpResponse # gians TODELETE DEBUG
 from django.shortcuts import render, redirect
-import logging # gians LOG TODO not working properly
-
+import logging 
 from uploadimg.forms import ImageForm
+from upload.settings import LOGGING
+import datetime
 
 # gians: libraries for AI
 import numpy as np
 import tensorflow.compat.v1 as tf
 import cv2 as cv
-#from tkinter import Image # gians TODELETE???
+
+IMAGE_TEMP = "img.jpg"
+
+def handl_uploaded_file(f, fname=IMAGE_TEMP):
+	with open(fname, 'wb+') as destination:
+		for chunk in f.chunks():
+			destination.write(chunk)
 
 def index(request):
     """Process images uploaded by users"""
-    logging.basicConfig(level=logging.INFO)
+
+    # create logger
     logger = logging.getLogger('gians')
+    logger.setLevel(logging.DEBUG)
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s:%(message)s')
+    # add formatter to ch
+    ch.setFormatter(formatter)
+    # add ch to logger
+    logger.addHandler(ch)
+    logger.info("Starting")
     
     # TODO use name of input file and use dynamic
     img_output_path = "./uploadimg/static/"
@@ -26,10 +67,12 @@ def index(request):
                         
             # Get the current instance object to display in the template
             img_obj = form.instance
-            
-            img_output_fname = img_obj.title + "_new.jpg" #TODO HARDCODED add datetime instead?
+            img_output_fname = img_obj.title + "_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
 
-            park_detection(img_obj, img_output_path, img_output_fname)
+            handl_uploaded_file(request.FILES['image'])
+            img = cv.imread(IMAGE_TEMP)
+
+            park_detection(img, img_output_path, img_output_fname)
             
             try:
                 img_output = open(img_output_path + img_output_fname, "rb")
@@ -48,16 +91,14 @@ def index(request):
     return render(request, 'index.html', {'form': form})
 
 
-def park_detection(img_obj, img_output_path, img_output_fname):
+def park_detection(img, img_output_path, img_output_fname):
+    global logger
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('gians')
     template_name = 'main/index.html'
 
     # Example for Tensorflow 2.0
     tf.disable_v2_behavior()
-
-    logger.info("park_detection()") #TODO not working
-    logger.info(img_output_fname) #TODO not working
 
     # Read the graph.
     with tf.io.gfile.GFile('frozen_inference_graph.pb', 'rb') as f:
@@ -69,9 +110,6 @@ def park_detection(img_obj, img_output_path, img_output_fname):
         sess.graph.as_default()
         tf.import_graph_def(graph_def, name='')
 
-        # Read and preprocess an image.
-        img = cv.imread('example.jpg') #TODO HARDCODED
-        #img = cv.imread(img_obj)
         rows = img.shape[0]
         cols = img.shape[1]
         inp = cv.resize(img, (300, 300))
